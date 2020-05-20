873e499b8528c8a03f8075aa77961b9ec6f4f9cf
Harsh-godzilla <62756451+Harsh-godzilla@users.noreply.github.com>
2020-05-20 16:53:55 +0530

    Add files via upload

---
+++ Contactusthrough.php
@@ -0,0 +1,17 @@
+<?php
+// variable setting
+$name = $_REQUEST['name'];
+$email = $_REQUEST['Email'];
+$message = $_REQUEST['Message'];
+
+//check input fields
+if (empty($name) & empty($email) & empty($message))
+{
+    echo "Please Fill All The Fields";
+}
+else
+{
+    mail("harshch410@gmail.com", "Harsh Site", $message , "From: $name <$email>");
+    echo "<script type='text/javascript'>alert('Your Message Sent Successfully');
+        window.history.log(-1);
+        </script>
